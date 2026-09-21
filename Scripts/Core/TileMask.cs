609c8907cf0a0d5ca9205642a857a7935fe7261a
andorgast <andorgast@gmail.com>
2026-09-21 14:24:50 +0200

    Adding characters and a couple attacks

--- Scripts/Core/TileMask.cs
+++ Scripts/Core/TileMask.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Godot;
 
 namespace Iternia.Scripts.Core;
 
@@ -26,30 +25,15 @@ public class TileMask
             if (rows[collum].Length != 3)
                 throw new ArgumentException("Each row in TileMask string must have exactly 3 characters.", nameof(mask));
 
-            for (int row = 0; row < 3; row++)
+            for (int rank = 0; rank < 3; rank++)
             {
-                if (rows[collum][row] != '.')
+                if (rows[collum][rank] != '.')
                 {
-                    positions.Add(new GridPos(row, collum));
+                    positions.Add(new GridPos(rank, collum));
                 }
             }
         }
         
-        return new TileMask(positions);
-    }
-    
-    public static TileMask Parse(Basis mask)
-    {
-        List<GridPos> positions = [];
-        if(mask.Row0.X != 0) positions.Add(new GridPos(0, 0));
-        if(mask.Row1.X != 0) positions.Add(new GridPos(1, 0));
-        if(mask.Row2.X != 0) positions.Add(new GridPos(2, 0));
-        if(mask.Row0.Y != 0) positions.Add(new GridPos(0, 1));
-        if(mask.Row1.Y != 0) positions.Add(new GridPos(1, 1));
-        if(mask.Row2.Y != 0) positions.Add(new GridPos(2, 1));
-        if(mask.Row0.Z != 0) positions.Add(new GridPos(0, 2));
-        if(mask.Row1.Z != 0) positions.Add(new GridPos(1, 2));
-        if(mask.Row2.Z != 0) positions.Add(new GridPos(2, 2));
         return new TileMask(positions);
     }
 
